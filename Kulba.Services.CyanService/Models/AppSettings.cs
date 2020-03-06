59b5b8be7bd2cbfa120840597c5b162804110bb0
Jim Kulba <JKulba@alphatecspine.com>
2020-03-06 14:05:30 -0800

    Add some settings props

---
+++ Kulba.Services.CyanService/Models/AppSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kulba.Services.CyanService.Models
+{
+    public class AppSettings
+    {
+        public string ConsoleTitle { get; set; }
+        public string Name { get; set; }
+        public string IpAddress { get; set; }
+        public string Port { get; set; }
+        public string  Environment { get; set; }
+    }
+}
